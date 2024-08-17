baf27816641e5240a58db3ab348dfb7b671f118f
eugene <eugene@localhost.localdomain>
2024-08-17 23:48:14 +0300

    Add logger and add http rate limit

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.22.1
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0
-	github.com/go-chi/docgen v1.3.0
+	github.com/go-chi/httprate v0.12.1
 	github.com/go-chi/render v1.0.3
+	gorm.io/gorm v1.25.11
 )
 
-require github.com/ajg/form v1.5.1 // indirect
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"os/exec"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httprate"
 	"github.com/go-chi/render"
 )
 
@@ -32,7 +35,30 @@ func checkCredentials(username, password string) bool {
 	return username == "admin" && password == "password"
 }
 
+func LoggerMiddleware(logger *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			t1 := time.Now()
+			defer func() {
+				logger.Info("request completed",
+					slog.String("method", r.Method),
+					slog.String("path", r.URL.Path),
+					slog.Int("status", ww.Status()),
+					slog.Int("size", ww.BytesWritten()),
+					slog.Duration("duration", time.Since(t1)),
+				)
+			}()
+
+			next.ServeHTTP(ww, r)
+		})
+	}
+}
+
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	flag.Parse()
 
 	r := chi.NewRouter()
@@ -42,7 +68,9 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.URLFormat)
+	r.Use(httprate.LimitByIP(100, time.Minute))
 	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(LoggerMiddleware(logger))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root."))
@@ -56,15 +84,15 @@ func main() {
 		panic("test")
 	})
 
-	// RESTy routes for "articles" resource
 	r.Route("/ips", func(r chi.Router) {
 		r.Post("/", AddIPtoFirewall)
 	})
-	fmt.Println("Server is running on :3333")
+	logger.Info("Server is running on :3333")
 	http.ListenAndServe(":3333", r)
 }
 
 func AddIPtoFirewall(w http.ResponseWriter, r *http.Request) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	var ip Firewall
 
 	err := json.NewDecoder(r.Body).Decode(&ip)
@@ -75,26 +103,28 @@ func AddIPtoFirewall(w http.ResponseWriter, r *http.Request) {
 	}
 
 	addIPTablesRule := exec.Command("iptables", "-I", "INPUT", "2", "-p", "tcp", "-s", ip.IP, "--dport", "1081", "-j", "ACCEPT")
-	output, err := addIPTablesRule.CombinedOutput()
+	_, err = addIPTablesRule.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Error executing command: %v\n", err)
+		logger.Error("Error executing command: %v\n", err)
+		//fmt.Printf("Error executing command: %v\n", err)
 		if exitError, ok := err.(*exec.ExitError); ok {
-			fmt.Printf("Command exited with non-zero status: %d\n", exitError.ExitCode())
+			//fmt.Printf("Command exited with non-zero status: %d\n", exitError.ExitCode())
+			logger.Error("Command exited with non-zero status: %d\n", exitError.ExitCode())
 		}
 		return
 	}
+	logger.Info("iptables added", ip.IP, "at chain rules")
 
 	saveIPTablesRule := exec.Command("sh", "-c", "iptables-save > /etc/iptables/rules.v4")
-	output1, err := saveIPTablesRule.CombinedOutput()
+	_, err = saveIPTablesRule.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Error executing command: %v\n", err)
+		logger.Error("Error executing command: %v\n", err)
 		if exitError, ok := err.(*exec.ExitError); ok {
-			fmt.Printf("Command exited with non-zero status: %d\n", exitError.ExitCode())
+			logger.Error("Command exited with non-zero status: %d\n", exitError.ExitCode())
 		}
 		return
 	}
 
-	fmt.Printf("Command output:\n%s\n", output)
-	fmt.Printf("Command output:\n%s\n", output1)
+	logger.Info("iptables saved rules")
 	render.Status(r, http.StatusCreated)
 }
